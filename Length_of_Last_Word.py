40536d4a2ceadddc3eb4507b8d35388552733866
Danyal Abbas <abasdaniel61@gmail.com>
2024-02-11 14:14:44 +0500

    length-of-last-word

--- Length_of_Last_Word.py
+++ Length_of_Last_Word.py
@@ -9,8 +9,7 @@ for pos, i in enumerate(s[::-1]):
         if len(s) == 1:
             break
         else:
-            print(pos)
-            if (s[::-1][pos] == s[0] and pos+1 == len(s)) or s[::-1][pos+1] == " ":
+            if pos+1 == len(s) or s[::-1][pos+1] == " ":
                 break
     
 print(count)
