e8b3d0d494b09901ee4e26b0ac70dd9eedcf855d
Danyal Abbas <abasdaniel61@gmail.com>
2024-03-02 11:41:14 +0500

    Update Best_Time_to_Buy_and_Sell_Stock.py

--- Best_Time_to_Buy_and_Sell_Stock.py
+++ Best_Time_to_Buy_and_Sell_Stock.py
@@ -3,24 +3,14 @@ prices = [7,1,5,3,6,4]
 import time
 
 def foo(prices : list):
-    temp = 0
-    num = 0
-    pos2 = 0
-    for pos1, i in enumerate(prices):
-        for j in prices[pos1+1::]:
-            if j < i:
-                num = j
-                pos2 = pos1+2
-                break
-            # print(f"{j} - {i}")
-            if (j - i) > temp:
-                temp = j-i
-        if (max(prices[pos2::]) - num) > temp:
-            print(f"{max(prices[pos2::])} - {num} > {temp} ")
-            temp = max(prices[pos2::]) - num
-            
+    min_num = prices[0]
+    max_num = 0
+    for i in prices[1::]:
+        max_num = max(max_num, i - min_num)
+        min_num = min(min_num, i)
+        
+    return max_num
     
-    return temp
 
 t1 = time.time()
 print(foo(prices))
