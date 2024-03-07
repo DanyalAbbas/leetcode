2ae3977aba38a433f0ef431a4c826b7b1fb1e1cc
Danyal Abbas <abasdaniel61@gmail.com>
2024-03-07 14:44:01 +0500

    Create Rotate_String.py

---
+++ Rotate_String.py
@@ -0,0 +1,15 @@
+s = "abcde"
+goal = "abced"
+
+def foo(s : str, goal : str):
+    l = list(s)
+    for i in l:
+        l.append(l.pop(0))
+        test = "".join(l)
+        if test == goal:
+            return True
+    return False
+
+
+print(foo(s,goal))
+        
