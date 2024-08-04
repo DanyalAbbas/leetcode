cf79f128448caf9071c232cbffc865d4da843969
Danyal Abbas <abasdaniel61@gmail.com>
2024-08-04 21:15:37 +0500

    Update Word_Pattern.py

--- Word_Pattern.py
+++ Word_Pattern.py
@@ -1,8 +1,25 @@
-pattern = "abba"
-s = "dog cat cat dog"
+pattern = "deadbeef"
+s = "d e a d b e e f"
 
 def foo(pattern : str, s : str):
-    for i in zip(pattern,s.split()):
-        print(i)
+    d = {}
+    count = 0
 
-foo(pattern,s)
+    if len(s.split()) == len(pattern):
+        pass
+    else:
+        return False
+
+    for i in pattern:
+        if i not in d.values() and s.split()[count] not in d.keys():
+            d[s.split()[count]] = i
+        count += 1
+    for pos, i in enumerate(s.split()):
+        if d.get(i) == pattern[pos]:
+            if pos == pattern[pos]:
+                pass
+        else:
+            return False
+    return True
+       
+print(foo(pattern,s))
