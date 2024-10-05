d2029dfe4f369017d2647020c8953c64e208e920
Danyal Abbas <abasdaniel61@gmail.com>
2024-10-05 11:45:41 +0500

    Create Reverse_Integer.py

---
+++ Reverse_Integer.py
@@ -0,0 +1,13 @@
+x = 123
+def foo(x : int) -> int:
+    Converted_Value = 0
+    if x < 0:
+        Converted_Value = int(str(x)[1:][::-1]) * -1
+    else:
+        Converted_Value = int(str(x)[::-1])
+    
+    if Converted_Value > 2 ** 31 - 1 or Converted_Value < -2 ** 31:
+        return 0
+    return Converted_Value
+
+print(foo(x))
