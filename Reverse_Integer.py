952617a3b60f268f4c1614b447426381cde7ed2c
Danyal Abbas <abasdaniel61@gmail.com>
2024-10-05 11:48:47 +0500

    Update Reverse_Integer.py

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,11 +1,11 @@
-x = 123
+x = 120
 def foo(x : int) -> int:
     Converted_Value = 0
     if x < 0:
         Converted_Value = int(str(x)[1:][::-1]) * -1
     else:
         Converted_Value = int(str(x)[::-1])
-    
+
     if Converted_Value > 2 ** 31 - 1 or Converted_Value < -2 ** 31:
         return 0
     return Converted_Value
