4341817870fd98eb477fb8d9660b7a6278106aae
Danyal Abbas <abasdaniel61@gmail.com>
2024-10-10 20:59:40 +0500

    Create Fizz_Buzz.py

---
+++ Fizz_Buzz.py
@@ -0,0 +1,17 @@
+n = 5
+
+def foo(n : int) -> list[str]:
+    l = []
+    for i in range(1, n+1):
+        if (i%3 == 0 and i%5 == 0):
+            l.append("FizzBuzz")
+        elif i%3 == 0:
+            l.append("Fizz")
+        elif i%5 == 0:
+            l.append("Buzz")
+        else:
+            l.append(str(i))
+    return l
+
+
+print(foo(n))
