049d11d1d1bf49bfb3a493701556f90c5add846e
Danyal Abbas <abasdaniel61@gmail.com>
2024-10-23 22:20:36 +0500

    Update Ugly_Number.py

---
+++ Ugly_Number.py
@@ -0,0 +1,25 @@
+n = 12
+
+def foo(n : int) -> bool:
+    factors = [2,3,5]
+
+    Number = n
+    UglyNumber = 1
+    factor = 2
+    
+    i = 0
+    while Number > i:
+        if UglyNumber == Number:
+            return True
+        if n % factor == 0 and factor in factors:
+            n = n//factor
+            UglyNumber *= factor
+        else:
+            factor +=1
+        i += 1
+    
+    return True if Number <=2 and Number > 0 else False
+
+            
+
+print(foo(n))
