ef0e54d84e816725db7789facda7c1d19ef6c0ea
Danyal Abbas <abasdaniel61@gmail.com>
2024-10-24 10:31:32 +0500

    hehe

--- two_sum.py
+++ two_sum.py
@@ -1,13 +1,13 @@
-nums = [3,2,3]
-target = 6
-p = []
-for pos1,i in enumerate(nums):
-    for pos2, j in enumerate(nums[1::]):
-            if i + j == target:
-                if pos1 != pos2+1:
-                    p.append(pos1)
-                    p.append(pos2+1)
-                if len(p) == 2:
-                    nums.clear()
-                break
-print(p)
+nums = [2,7,11,15]
+target = 9
+
+def foo(nums : list[int], target : int) -> list[int]:
+    for i in range(len(nums)):
+        for j in range(len(nums)):
+            if i == j:
+                continue
+            elif nums[i] + nums[j] == target:
+                return [i,j]
+
+
+print(foo(nums, target))
