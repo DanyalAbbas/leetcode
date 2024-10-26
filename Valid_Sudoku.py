afb68277abb03bc6971d25a3e92f7a5bb3bf61f1
Danyal Abbas <abasdaniel61@gmail.com>
2024-10-26 16:09:19 +0500

    Update Valid_Sudoku.py

--- Valid_Sudoku.py
+++ Valid_Sudoku.py
@@ -1,12 +1,12 @@
 board = [["5","3",".",".","7",".",".",".","."]
-,["6",".",".","1","9","5",".",".","."]
-,[".","9","8",".",".",".",".","6","."]
-,["8",".",".",".","6",".",".",".","3"]
-,["4",".",".","8",".","3",".",".","1"]
-,["7",".",".",".","2",".",".",".","6"]
-,[".","6",".",".",".",".","2","8","."]
-,[".",".",".","4","1","9",".",".","5"]
-,[".",".",".",".","8",".",".","7","9"]]
+        ,["6",".",".","1","9","5",".",".","."]
+        ,[".","9","8",".",".",".",".","6","."]
+        ,["8",".",".",".","6",".",".",".","3"]
+        ,["4",".",".","8",".","3",".",".","1"]
+        ,["7",".",".",".","2",".",".",".","6"]
+        ,[".","6",".",".",".",".","2","8","."]
+        ,[".",".",".","4","1","9",".",".","5"]
+        ,[".",".",".",".","8",".",".","7","9"]]
 
 def foo(board : list[list[str]]) -> bool:
 
@@ -14,19 +14,34 @@ def foo(board : list[list[str]]) -> bool:
     Columns = True
     Grid3 = True
     board1 = board[:]
+    board2 = board[:]
+    board3 = board[:]
+
+    board3 = [board[j][i:i+3] for i in range(0,len(board),3) for j in range(9)]
+    gridbox = []
+    rand = []
+    count = 0
+    for i in board3:
+        rand += i
+        count += 1
+        if count % 3 == 0:
+            gridbox.append(rand)
+            rand = []
 
     for i in range(len(board1)):
         board1[i] = [board1[i][:].pop(j) for j in range(len(board1[i])) if board1[i][j] != "."]
+        board2[i] = [board[k][i] for k in range(len(board)) if board[k][i] != "."]
+        gridbox[i] = [j for j in gridbox[i] if j != "."]
     
-    for row in range(len(board1)):
-        if len(set(board1[row])) != len(board1[row]):
+    for i in range(len(board)):
+        if len(set(board1[i])) != len(board1[i]):
             Rows = False
-    
-    for column
+        if len(set(board2[i])) != len(board2[i]):
+            Columns = False
+        if len(set(gridbox[i])) != len(gridbox[i]):
+            Grid3 = False
 
-
-    
-    return board
+    return Rows and Columns and Grid3
 
 
 print(foo(board))
