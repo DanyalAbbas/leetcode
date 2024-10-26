cf8a9c349c893741afad0bb67269a80101c0188c
Danyal Abbas <abasdaniel61@gmail.com>
2024-10-26 16:16:35 +0500

    Update Valid_Sudoku.py

--- Valid_Sudoku.py
+++ Valid_Sudoku.py
@@ -13,30 +13,30 @@ def foo(board : list[list[str]]) -> bool:
     Rows = True
     Columns = True
     Grid3 = True
-    board1 = board[:]
-    board2 = board[:]
-    board3 = board[:]
+    boardRow = board[:]
+    boardColumn = board[:]
+    boardGrid = board[:]
     
-    board3 = [board[j][i:i+3] for i in range(0,len(board),3) for j in range(9)]
+    boardGrid = [board[j][i:i+3] for i in range(0,len(board),3) for j in range(9)]
     gridbox = []
     rand = []
     count = 0
-    for i in board3:
+    for i in boardGrid:
         rand += i
         count += 1
         if count % 3 == 0:
             gridbox.append(rand)
             rand = []
 
-    for i in range(len(board1)):
-        board1[i] = [board1[i][:].pop(j) for j in range(len(board1[i])) if board1[i][j] != "."]
-        board2[i] = [board[k][i] for k in range(len(board)) if board[k][i] != "."]
+    for i in range(len(boardRow)):
+        boardRow[i] = [boardRow[i][:].pop(j) for j in range(len(boardRow[i])) if boardRow[i][j] != "."]
+        boardColumn[i] = [board[k][i] for k in range(len(board)) if board[k][i] != "."]
         gridbox[i] = [j for j in gridbox[i] if j != "."]
     
     for i in range(len(board)):
-        if len(set(board1[i])) != len(board1[i]):
+        if len(set(boardRow[i])) != len(boardRow[i]):
             Rows = False
-        if len(set(board2[i])) != len(board2[i]):
+        if len(set(boardColumn[i])) != len(boardColumn[i]):
             Columns = False
         if len(set(gridbox[i])) != len(gridbox[i]):
             Grid3 = False
