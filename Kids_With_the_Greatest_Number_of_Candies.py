6b8dcbd5fcecf1611cd8bb34a1b1832f4355f762
Danyal Abbas <abasdaniel61@gmail.com>
2024-11-14 13:18:11 +0500

    added Kids with Greatest Number of Candies question of leetcode

---
+++ Kids_With_the_Greatest_Number_of_Candies.py
@@ -0,0 +1,16 @@
+candies = [2,3,5,1,3]
+extraCandies = 3
+
+def foo(candies : list[int], extraCandies : int) -> list[bool]:
+    NewCandies = []
+    for i in candies:
+        if max(i+extraCandies, max(candies)) == i+extraCandies:
+            NewCandies.append(True)
+        else:
+            NewCandies.append(False)
+    
+    return NewCandies
+
+
+print(foo(candies, extraCandies))
+            
