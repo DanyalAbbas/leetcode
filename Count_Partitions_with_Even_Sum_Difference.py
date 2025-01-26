41b36784dca50f3a0cb31963f590588bbeabcc6c
Danyal Abbas <abasdaniel61@gmail.com>
2025-01-26 22:45:12 +0500

    Create Count_Partitions_with_Even_Sum_Difference.py

---
+++ Count_Partitions_with_Even_Sum_Difference.py
@@ -0,0 +1,15 @@
+nums = [2,4,6,8]
+
+def foo(nums : list[int]) -> int:
+    Length = len(nums)
+    NoOfPartitions = 0
+    for i in range(Length-1):
+        print(nums[0:i+1])
+        print(nums[i+1:Length])
+        if (sum(nums[0:i+1]) - sum(nums[i+1:Length])) % 2 == 0:
+            NoOfPartitions += 1
+    
+    return NoOfPartitions
+
+
+print(foo(nums))
