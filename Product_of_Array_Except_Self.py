df86c17602d35c0a6f4b41b58385732656096481
Danyal Abbas <abasdaniel61@gmail.com>
2025-01-27 22:34:40 +0500

    Update Product_of_Array_Except_Self.py

--- Product_of_Array_Except_Self.py
+++ Product_of_Array_Except_Self.py
@@ -1,15 +1,23 @@
 import math
-nums = [1,3,2,4,4,4,4,4,4]
+nums = [-1,1,0,-3,3]
 
 def foo(nums : list[int]) -> list[int]:
-    Answer = []
-    for i in range(len(nums)):
-        nums_copy = nums[:]
-        nums_copy.pop(i)
-        Answer.append(math.prod(nums_copy))
-    return Answer
+    numscopy = nums[:]
+    ZeroCount = nums.count(0)
+    for i in range(ZeroCount):
+        nums.remove(0)
+    AllProduct = math.prod(nums)
+    ProductedValues = []
+    if ZeroCount > 1:
+        return [0] * len(numscopy)
+    elif ZeroCount == 0:
+        for i in range(len(nums)):
+            ProductedValues.append(int(AllProduct/nums[i]))
+    elif ZeroCount == 1:
+        ProductedValues = [0] *  len(numscopy)
+        ProductedValues[numscopy.index(0)] = AllProduct
 
-nums.remove(4)
+    return ProductedValues
 
-print(sorted(nums, reverse= True))
 print(foo(nums))
+    
