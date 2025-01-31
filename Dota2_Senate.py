070eb9a550012dd7c5bb837492305bb2d907ac53
Danyal Abbas <abasdaniel61@gmail.com>
2025-01-31 21:10:04 +0500

    Made Dota2_Senate.py a bit cleaner

--- Dota2_Senate.py
+++ Dota2_Senate.py
@@ -2,25 +2,27 @@ senate = "RDD"
 
 def foo(senate : str) -> str:
     senate = list(senate)
-    r = senate.count("R")
-    d = senate.count("D")
+    RadiantCount = senate.count("R")
+    DireCount = senate.count("D")
     i = 0
-    while (d != len(senate)) or (r != len(senate)):
+    while (DireCount != len(senate)) or (RadiantCount != len(senate)):
         print(senate)
-        if senate[i] == "D" and d == len(senate):
-            return "Dire"
-        elif senate[i] == "D":
-            senate.append(senate.pop(0))
-            senate.remove("R")
-            i -= 1
-            r -= 1
-        elif senate[i] == "R" and r == len(senate):
-            return "Radiant"
-        elif senate[i] == "R":
-            senate.append(senate.pop(0))
-            senate.remove("D")
-            i -= 1
-            d -= 1
+        if senate[i] == "D":
+            if DireCount == len(senate):
+                return "Dire"
+            else:
+                senate.append(senate.pop(0))
+                senate.remove("R")
+                i -= 1
+                RadiantCount -= 1
+        else: 
+            if RadiantCount == len(senate):
+                return "Radiant"
+            else:
+                senate.append(senate.pop(0))
+                senate.remove("D")
+                i -= 1
+                DireCount -= 1
         i +=1
             
 
