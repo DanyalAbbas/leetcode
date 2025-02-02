676b13f5ed044ad2d63877ec915250c5e59134e1
Danyal Abbas <abasdaniel61@gmail.com>
2025-02-02 16:20:50 +0500

    Update Destination_City.py

--- Destination_City.py
+++ Destination_City.py
@@ -1,11 +1,12 @@
 paths = [["London","New York"],["New York","Lima"],["Lima","Sao Paulo"]]
 
 def foo(paths : list[list[str]]) -> str:
-    NewList = []
-    for i in zip(*paths): NewList.append(i)
-
-    for i in NewList[1]:
-        if i not in NewList[0]:
+    paths = zip(*paths)
+    StartingCities = next(paths)
+    DestinationCities =  next(paths)
+    
+    for i in DestinationCities:
+        if i not in StartingCities:
             return i
     
 
