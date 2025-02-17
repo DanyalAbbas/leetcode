d51e756646b3c2d0a38f1ba4254e99fff5566e86
Danyal Abbas <abasdaniel61@gmail.com>
2025-02-17 23:59:04 +0500

    More Optimized Remove Duplicates from Sorted Array II

--- Remove_Duplicates_from_Sorted_Array_II.py
+++ Remove_Duplicates_from_Sorted_Array_II.py
@@ -1,11 +1,17 @@
-nums = [0,0,0,0,0]
+nums = [1,1,1,2,2,3]
 
 def foo(nums : list[int]) -> int:
     i = 0
-    while i < len(nums):
-        if nums.count(nums[i]) > 2:
-            nums.pop(i)
-            i-=1
+    counter = 1
+    while i < len(nums)-1:
+        CurrValue = nums[i]
+        if nums[i+1] == CurrValue:
+            if counter > 1:
+                nums.pop(i)
+                i -= 1
+            counter += 1
+        if nums[i+1] != CurrValue:
+            counter = 1
         i += 1
     
     return len(nums)
