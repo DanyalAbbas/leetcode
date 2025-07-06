4086de14053d917825600818a3adf6bf2675db61
Danyal Abbas <abasdaniel61@gmail.com>
2025-07-06 14:40:46 +0500

    Maximum subarray

--- Maximum_Subarray.py
+++ Maximum_Subarray.py
@@ -1,4 +1,13 @@
 nums = [-2,1,-3,4,-1,2,1,-5,4]
 
 def foo(nums : list[int]):
-    
+    minimumNum = -10000000
+    num = 0
+    for i in nums:
+        num += i
+        minimumNum = max(minimumNum, num)
+        if num < 0: num=0
+    return minimumNum
+
+
+print(foo(nums))
