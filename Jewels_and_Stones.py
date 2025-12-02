5d0ff6f3c707facfe9673663bea44d1d983be3fe
Danyal Abbas <abasdaniel61@gmail.com>
2025-12-03 01:12:08 +0500

    idk man

--- Jewels_and_Stones.py
+++ Jewels_and_Stones.py
@@ -4,7 +4,7 @@ stones = "aAAbbbb"
 def foo(jewels : str, stones : str) -> int:
     n = 0
     for i in stones:
-        if i in jewels:
+        if i in set(jewels):
             n += 1
 
     return n
